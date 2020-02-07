77823dd2e71ec1616cef2f5b00996eaa461dbda0
Petro Kulybaba <ahurtep@gmail.com>
2020-02-07 17:11:53 +0200

    added handlers_test.go

---
+++ tests/handlers_test.go
@@ -0,0 +1,18 @@
+package handlers_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/petrokulybaba/go-basic-framework/configs"
+)
+
+func TestIndexHandler(t *testing.T) {
+	response, err := http.Get("http://localhost" + configs.PORT)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if status := response.StatusCode; status != http.StatusOK {
+		t.Errorf("Wrong response status code: got %d want %d", status, http.StatusOK)
+	}
+}
